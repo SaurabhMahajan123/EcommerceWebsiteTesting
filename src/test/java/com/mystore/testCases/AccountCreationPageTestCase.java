546bd30ad8583b05e57ef0f9a3974871f01d723f
saurabh mahajan <saurabhmahajan1997@gmail.com>
2024-08-12 10:42:08 +0530

    accountCreationTestCasesDone

---
+++ src/test/java/com/mystore/testCases/AccountCreationPageTestCase.java
@@ -0,0 +1,38 @@
+package com.mystore.testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.mystore.base.BaseClass;
+import com.mystore.pageObjects.AccountCreationPage;
+import com.mystore.pageObjects.IndexPage;
+import com.mystore.pageObjects.LoginPage;
+
+public class AccountCreationPageTestCase extends BaseClass {
+	IndexPage indexPage;
+	LoginPage loginPage;
+	AccountCreationPage accountCreation;
+	
+	@BeforeMethod
+	public void setup() {
+		launchApp();
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void verifyCreateAccountPageTest () throws Throwable {
+		
+		indexPage= new IndexPage();
+		loginPage=indexPage.clickOnSignIn();
+		accountCreation=loginPage.createNewAccount("dfjkekr@xyz.com");
+		boolean result=accountCreation.validateAcountCreatePage();
+		Assert.assertTrue(result);
+		
+	}
+}
