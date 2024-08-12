223cf8087dfb9ca0d6246b11aebd4c10076b58b5
saurabh mahajan <saurabhmahajan1997@gmail.com>
2024-08-12 10:51:28 +0530

    searchResultTestCaseDone

---
+++ src/test/java/com/mystore/testCases/SearchResultPageTestCase.java
@@ -0,0 +1,35 @@
+package com.mystore.testCases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.mystore.base.BaseClass;
+import com.mystore.pageObjects.IndexPage;
+import com.mystore.pageObjects.SearchResultPage;
+
+public class SearchResultPageTestCase extends BaseClass{
+
+	IndexPage indexPage;
+	SearchResultPage searchResult;
+	
+	@BeforeMethod
+	public void setup() {
+		launchApp();
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+	
+	@Test
+	public void productAvialabilityTest() throws Throwable {
+		indexPage = new IndexPage();
+		searchResult = indexPage.searchProduct("t-shirt");
+		boolean result = searchResult.isProductAvailable();
+		Assert.assertTrue(result);
+	
+	}
+}
